60bed38e68ecbf4d62d1f097b90ffa3cd9b9d84e
Salix <saulpachecotrilles.@gmail.com>
2021-11-29 21:07:20 +0100

    nav.part actualizado

--- views/index.view.php
+++ views/index.view.php
@@ -3,68 +3,6 @@
   include __DIR__ . "/partials/nav.part.php";
 ?>
 
-		
-
-		<!-- Falta Cambiar las funciones de js -->
-
-		<div id="id_login" class="modal">
-			<form class="modal__content animate" action="#" method="post">
-				<div class="img__container">
-					<span onclick="document.getElementById('id_login').style.display='none'" class="close close__login" title="Close Modal">&times;</span>
-					<img src="./img/dado20.PNG" alt="Avatar" class="avatar">
-				</div>
-			
-				<div class="container__login">
-					<label for="uname"><b>Username</b></label>
-					<input type="text" class="entrada entrada__text__login" placeholder="Enter Username" name="uname" required>
-			
-					<label for="psw"><b>Password</b></label>
-					<input type="password" class="entrada entrada__psw__login" placeholder="Enter Password" name="psw" required>
-					
-					<button class="btn btn__login" type="submit">Login</button>
-					<label>
-						<input type="checkbox" checked="checked" name="remember"> Remember me
-					</label>
-				</div>
-		  
-			  <div class="container__send clearfix">
-				<button type="button" onclick="document.getElementById('id_login').style.display='none'" class="btn btn__cancel__login">Cancel</button>
-				<span class="psw">Forgot <a href="#">password?</a></span>
-			  </div>
-			</form>
-		</div>
-		<div id="id_sign_up" class="modal">
-			<span onclick="document.getElementById('id_sign_up').style.display='none'" class="close close__sign__up" title="Close Modal">&times;</span>
-			<form class="modal__content animate" action="#">
-				<div class="container__sing__up">
-					<h1>Sign Up</h1><br>
-					<p>Please fill in this form to create an account.</p><br>
-					<hr>
-					<label for="email"><b>Email</b></label>
-					<input type="text" class="entrada entrada__email__sign_up" placeholder="Enter Email" name="email" required>
-			
-					<label for="psw"><b>Password</b></label>
-					<input type="password" class="entrada entrada__psw__sign_up" placeholder="Enter Password" name="psw" required>
-			
-					<label for="psw-repeat"><b>Repeat Password</b></label>
-					<input type="password" class="entrada entrada__psw__repeat__sign_up" placeholder="Repeat Password" name="psw-repeat" required>
-					
-					<label>
-						<input type="checkbox" checked="checked" class="checkbox__sign__up" name="remember"> Remember me
-					</label>
-			
-					<p>By creating an account you agree to our <a href="#" class="blue__sing__up">Terms & Privacy</a>.</p>
-				</div>
-
-				<div class="container__send clearfix">
-					<button type="button" onclick="document.getElementById('id_sign_up').style.display='none'" class="btn cancel__sign__up">Cancel</button>
-					<button type="submit" class="btn btn__sign__up">Sign Up</button>
-				</div>
-			</form>
-		</div>
-
-		<div class="clearfix"></div>
-
 		<div class="container">
 
 			<div class="card">
@@ -116,7 +54,7 @@
                 <div><textarea type="text" class="card__textarea" placeholder="Notas"></textarea></div>
 			</div>
 		</div>
-		
+
 		<div class="container">
 
 			<div class="card">
